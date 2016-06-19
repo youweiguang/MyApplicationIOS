fd3bd82ef8d83d3c65478cde5f2a515defff0a72
尤伟光 <youweiguang@youweiguangdemac.local>
2016-06-19 16:09:01 +0800

    增加登录页面

---
+++ MyApplicationIOS/MyApplicationIOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyApplicationIOS
+//
+//  Created by 尤伟光 on 16/6/19.
+//  Copyright © 2016年 尤伟光. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyApplicationIOS/MyApplicationIOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyApplicationIOS
+//
+//  Created by 尤伟光 on 16/6/19.
+//  Copyright © 2016年 尤伟光. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
